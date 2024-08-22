d569dee64f06013fea7319cea836a0350fa5a976
DevFaith <waitherafaith@outlook.com>
2024-08-22 20:05:45 +0300

    * feat(App.jsx): add App component with routes and pages

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './Pages/HomePage';
+import ResourcesPage from './Pages/ResourcesPage';
+import CommunityPage from './Pages/CommunityPage';
+import AboutPage from './Pages/AboutPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/resources" element={<ResourcesPage />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
